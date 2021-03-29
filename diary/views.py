3a53bf4744a976088ae1d608e5ae6cc8f2dd6a07
Dilyan Penev <dilyan.penev@student.manchester.ac.uk>
2021-03-30 00:24:14 +0300

    Create buttons for login and register

--- diary/views.py
+++ diary/views.py
@@ -4,10 +4,24 @@ from .models import Post
 
 
 def index(request):
-    return render(request, 'general/index.html')
+    if request.user.is_authenticated:
+        u = request.user
+    else:
+        u = None
+    params = {
+        'user': u,
+    }
+    return render(request, 'general/index.html', params)
 
 
 def all_posts(request):
+    if request.user.is_authenticated:
+        u = request.user
+    else:
+        u = None
     posts_list = Post.objects.all()
-    context = {'posts_list': posts_list}
+    context = {
+        'posts_list': posts_list,
+        'user': u
+    }
     return render(request, 'posts/allposts.html', context)
--- register/views.py
+++ register/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from .forms import RegisterForm
+from django.http import HttpResponseRedirect
 
 
 def register(response):
@@ -9,6 +10,8 @@ def register(response):
             form.save()
         return redirect("/posts")
     else:
+        if response.user.is_authenticated:
+            return HttpResponseRedirect("/")
         form = RegisterForm()
 
     return render(response, "register/register.html", {"form": form})
