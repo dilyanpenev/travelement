7e3d5bf6cc2839742ebcb82ccb864668c9b9e315
Dilyan Penev <dilyan.penev@student.manchester.ac.uk>
2021-03-30 11:08:26 +0300

    Add user field in post model

--- diary/models.py
+++ diary/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.TextField()
     pub_date = models.DateTimeField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.TextField()
 
     def __str__(self):
