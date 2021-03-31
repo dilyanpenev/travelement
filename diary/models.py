cda5512cc020d8176125240119ee88660baf3ff1
Dilyan Penev <dilyan.penev@student.manchester.ac.uk>
2021-03-31 20:52:44 +0300

    Add minimum length of title

--- diary/models.py
+++ diary/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.urls import reverse
+from django.core.validators import MinLengthValidator
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=60)
+    title = models.CharField(max_length=60, validators=[
+                             MinLengthValidator(1, message='Title has to be 1-60 chars')])
     pub_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
     description = models.CharField(max_length=600)
