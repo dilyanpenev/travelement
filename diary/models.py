d038ecf0dd3637bc13c146f08cbd5f6652714b04
Dilyan Penev <dilyan.penev@student.manchester.ac.uk>
2021-03-31 20:38:26 +0300

    Validate text fields

--- diary/models.py
+++ diary/models.py
@@ -1,23 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
-from django.utils import timezone
 from django.urls import reverse
 
 
-class PostManager(models.Manager):
-    def create_post(self, title, user, desc):
-        post = self.create(title=title, pub_date=timezone.now(),
-                           user=user, description=desc)
-        return post
-
-
 class Post(models.Model):
-    title = models.TextField()
+    title = models.CharField(max_length=60)
     pub_date = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
-    description = models.TextField()
-
-    objects = PostManager()
+    description = models.CharField(max_length=600)
 
     def __str__(self):
         return self.title
